c3011d0dd7da05cd70367364f7d2b732bb215936
RagV2C18472 <RagV2C18472@V2CP0082.vee2office.com>
2021-03-30 10:56:33 +0530

    Assessment-13

---
+++ collection.test/src/javacollection/Employee.java
@@ -0,0 +1,42 @@
+package javacollection;
+
+import java.util.ArrayList;
+
+ class details {
+	 
+    
+	int id;
+	String name;
+	Status status;
+	
+	enum Status
+	{
+		CONFORMED,PENDING,REJECTED
+	}
+
+	public details(int id, String name, Status status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.status = status;
+	}}
+ 
+  public class Employee
+  {
+	  
+
+	public static void main(String[] args) {
+		
+		ArrayList<details> al = new ArrayList<details>();
+		
+		al.add(new details (11,"vijay", details.Status.CONFORMED));
+		al.add(new details (12,"ajith", details.Status.PENDING));
+		al.add(new details (13,"ram", details.Status.REJECTED));
+		
+		for(details e : al)
+		{
+		System.out.println(e.id+ " "+e.name+ " "+e.status+ " ");
+		
+	}
+	}
+}
